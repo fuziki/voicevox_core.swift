3c171e74de496934d4a67950e27cd1c11f71765c
fuziki <hymne.shannon.projects@gmail.com>
2023-08-15 01:58:37 +0900

    Update Example

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -12,24 +12,16 @@ struct ContentView: View {
     @ObservedObject var vm = ContentViewModel()
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 32) {
+            TextField("", text: $vm.text)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                vm.onTap()
+            } label: {
+                Text("読み上げる")
+            }
         }
         .padding()
-        .onAppear {
-            vm.onAppear()
-        }
-        .onChange(of: vm.data) { data in
-            guard let data else { return }
-            let items = [data] as [Any]
-            let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
-            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-            let rootVC = windowScene?.windows.first?.rootViewController
-            rootVC?.present(activityVC, animated: true,completion: {})
-        }
     }
 }
 
--- Example/ContentViewModel.swift
+++ Example/ContentViewModel.swift
@@ -5,49 +5,57 @@
 //  Created by fuziki on 2023/08/14¥5.
 //
 
+import AVFoundation
 import Foundation
 import VoicevoxCore
 import libVoicevoxCore
 
 class ContentViewModel: ObservableObject {
-    @Published var data: Data? = nil
+    @Published var text: String = "この音声はアイフォーン上で生成されているのだ。"
 
-    func onAppear() {
-        print("version: \(VoicevoxCore.version)")
+    var synthesizer: Synthesizer? = nil
+    var player: AVAudioPlayer?
 
-        // OpenJtalkRc
-        let dictURL = Bundle.main.resourceURL!
-            .appendingPathComponent("open_jtalk_dic_utf_8-1.11")
-        let ojrc = OpenJtalkRc(openJtalkDic: dictURL)
+    func onTap() {
+        if synthesizer == nil {
+            print("version: \(VoicevoxCore.version)")
 
-        // UserDict
-        let dict = UserDict()
-        let world = UserDictWord(surface: "hoge", pronunciation: "フガ")
-        world.type = .commonNoun
-        world.priority = 10
-        let uuid = dict.add(word: world)
-        print("uuid: \(uuid)")
-        ojrc.use(userDict: dict)
+            // OpenJtalkRc
+            let dictURL = Bundle.main.resourceURL!
+                .appendingPathComponent("open_jtalk_dic_utf_8-1.11")
+            let ojrc = OpenJtalkRc(openJtalkDic: dictURL)
 
-        // Set Model Root
-        let modelURL = Bundle.main.resourceURL!
-            .appendingPathComponent("models")
-        VoicevoxCore.set(modelsRoot: modelURL)
+            // UserDict
+            let dict = UserDict()
+            let world = UserDictWord(surface: "hoge", pronunciation: "フガ")
+            world.type = .commonNoun
+            world.priority = 10
+            let uuid = dict.add(word: world)
+            print("uuid: \(uuid)")
+            ojrc.use(userDict: dict)
 
-        // Synthesizer
-        var initialize_options: VoicevoxInitializeOptions = voicevox_default_initialize_options
-        initialize_options.load_all_models = true
-        let synthesizer = Synthesizer(openJtalk: ojrc, options: initialize_options)
+            // Set Model Root
+            let modelURL = Bundle.main.resourceURL!
+                .appendingPathComponent("models")
+            VoicevoxCore.set(modelsRoot: modelURL)
 
-        // AudioQuery
-        let aq = synthesizer.audioQuery(text: "チョリース。世界。なのだヨ！hoge", styleId: 1, options: VoicevoxAudioQueryOptions(kana: false))
-        print("aq: \(aq)")
+            // Synthesizer
+            var initialize_options: VoicevoxInitializeOptions = voicevox_default_initialize_options
+            initialize_options.load_all_models = true
+            synthesizer = Synthesizer(openJtalk: ojrc, options: initialize_options)
+        }
 
-        // wav
-        data = synthesizer.synthesis(audioQuery: aq, styleId: 1, options: voicevox_default_synthesis_options)
+        let synthesizer = synthesizer!
 
-        // wav
-//        data = synthesizer.textToSpeech(text: "hogeなのさ、この世界は、hoge", styleId: 1, options: voicevox_default_tts_options)
+        let aq = synthesizer.audioQuery(text: text, styleId: 1, options: VoicevoxAudioQueryOptions(kana: false))
+        let wav = synthesizer.synthesis(audioQuery: aq, styleId: 1, options: voicevox_default_synthesis_options)
+//        let wav = synthesizer.textToSpeech(text: text, styleId: 1, options: voicevox_default_tts_options)
+
+        if player?.isPlaying == true {
+            player?.stop()
+        }
+        player = try? AVAudioPlayer(data: wav)
+        player?.play()
     }
 }
 
