73c0eefe13068736e1bf74f49bca2fc3a02d5f9a
fuziki <hymne.shannon.projects@gmail.com>
2023-08-15 01:11:18 +0900

    Create App Target

---
+++ Example/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Example
+//  
+//  Created by fuziki on 2023/08/15
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//  
+//  Created by fuziki on 2023/08/15
+//  
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
